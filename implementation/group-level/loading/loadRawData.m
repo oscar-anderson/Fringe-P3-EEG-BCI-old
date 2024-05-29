6938ca92e64a01d11df862c02225b6cf1cc9b5a4
Oscar Anderson <117745490+oscar-anderson@users.noreply.github.com>
2024-05-29 16:56:23 +0100

    Upload loadRawData.m

---
+++ implementation/group-level/loading/loadRawData.m
@@ -0,0 +1,20 @@
+function rawDataset = loadRawData(filenames, rawDataPath)
+
+cd(rawDataPath);
+
+numFiles = length(filenames);
+rawDataset = cell(1, numFiles);
+
+for iFile = 1:numFiles
+    dataFile = filenames{iFile};
+    cfg = [];
+    cfg.dataset = dataFile;
+    cfg.trialdef.eventtype = 'STATUS'; % Trials are marked as type 'STATUS'.
+    cfg.trialdef.eventvalue = 111:170; % Select relevant trials by markers.
+    cfg.trialdef.prestim = 0.5; % Specify time to include pre-probe.
+    cfg.trialdef.poststim = 1.5; % Specify time to include post-probe.
+    cfg = ft_definetrial(cfg);
+
+    % Load data into structs.
+    rawDataset{iFile} = ft_preprocessing(cfg);
+end
