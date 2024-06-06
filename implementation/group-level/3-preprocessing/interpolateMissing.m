44a8321a03d0c9700bf4900da54a1ef295d27eec
Oscar Anderson <117745490+oscar-anderson@users.noreply.github.com>
2024-06-06 09:44:14 +0100

    Update interpolateMissing.m

--- implementation/group-level/3-preprocessing/interpolateMissing.m
+++ implementation/group-level/3-preprocessing/interpolateMissing.m
@@ -1,21 +1,18 @@
-function interpolatedData = interpolateMissing(data, method)
+function interpolatedData = interpolateMissing(data, method, varargin)
 
-% Define neighbours.
-validChannels = {'Fp1'; 'AF3'; 'F7'; 'F3'; 'FC1'; 'FC5'; 'T7'; 'C3'; ...
-    'CP1'; 'CP5'; 'P7'; 'P3'; 'Pz'; 'PO3'; 'O1'; 'Oz'; 'O2'; 'Po4'; ...
-    'P4'; 'P8'; 'CP6'; 'CP2'; 'C4'; 'T8'; 'FC6'; 'FC2'; 'F4'; 'F8'; ...
-    'AF4'; 'Fp2'; 'Fz'; 'Cz'; 'A1'; 'A2'; 'LEOG'; 'REOG'; 'UEOG'; ...
-    'DEOG'; 'EXG7'; 'EXG8'};
+% THERE REMAINS AN ISSUE WHEREIN THE CHANNEL PO4 IN THE TEMPLATE IS
+% LABELLED Po4 IN THE DATA.
+%           - WHAT IS THE CAUSE OF THIS DISCREPANCY?
+%           - HOW TO FIX?
 
 cfg = [];
-cfg.channel = validChannels;
 cfg.method = 'template';
 cfg.template = 'biosemi32_neighb.mat';
-cfg.feedback = 'no';
 
-neighbours = ft_prepare_neighbours(cfg, data);
+neighbours = ft_prepare_neighbours(cfg);
 
 % Identify missing channels.
+validChannels = {neighbours(:).label};
 missingChannelsIdx = ~ismember(validChannels, data.label)';
 missingChannels = validChannels(missingChannelsIdx)';
 
@@ -28,9 +25,9 @@ cfg.trials = 'all';
 cfg.senstype = 'eeg';
 
 if strcmp(method, 'weighted')
-    cfg.elec = []; % FIX ----------------------------------------------- !
+    cfg.elec = varargin{1};
 elseif strcmp(method, 'spline') || strcmp(method, 'splat')
-    cfg.elec = []; % FIX ----------------------------------------------- !
+    cfg.elec = varargin{1};
     cfg.lambda = 1e-5;
     cfg.order = 4;
 elseif ~any(strcmp(method, {'weighted', 'average', 'spline', 'splat', 'nan'}))
@@ -39,4 +36,3 @@ end
 
 interpolatedData = ft_channelrepair(cfg, data);
 
-end
